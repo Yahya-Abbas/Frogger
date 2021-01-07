0be3450fd2d4adf409f0217245342d612260d187
Yahya-Abbas <58621691+Yahya-Abbas@users.noreply.github.com>
2021-01-07 16:11:37 +0200

    Added the pickups class

---
+++ PickUps.cpp
@@ -0,0 +1,56 @@
+#include "PickUps.h"
+
+PickUps::PickUps()
+{
+	T1.loadFromFile("PickupScore.png");
+	//PickupScore.setTexture(T1);
+	T2.loadFromFile("PickupLives.png");
+	//PickupLives.setTexture(T2);
+	T3.loadFromFile("PickupTime.png");
+	//PickupScore.setTexture(T3);
+	srand(time(NULL));
+	s = rand() % 3;
+}
+
+void PickUps::setPickupposition(RenderWindow &window)
+{
+
+	switch (s)
+	{
+	case 0:
+		Pickup.setTexture(T1);
+		Pickup.setScale(0.07, 0.07);
+		break;
+	case 1:
+		Pickup.setTexture(T2);
+		Pickup.setScale(0.07, 0.07);
+
+		break;
+	case 2:
+		Pickup.setTexture(T3);
+		Pickup.setScale(0.07, 0.07);
+		break;
+	}
+	Pickup.setPosition(xposition, yposition);
+}
+
+void PickUps::draw(RenderWindow &window)
+{
+	window.draw(Pickup);
+}
+
+Sprite PickUps::getBoundingRectangle()
+{
+	return Pickup;
+}
+
+bool PickUps::PickupIntersect(Player &player, RenderWindow &window)
+{
+	if (player.returnbody().getGlobalBounds().intersects(getBoundingRectangle().getGlobalBounds()))
+		return true;
+	else
+		return false;
+}
+PickUps::~PickUps()
+{
+}
---
+++ PickUps.h
@@ -0,0 +1,31 @@
+#pragma once
+#include <SFML/Graphics.hpp>
+#include "Player.h"
+using namespace sf;
+class PickUps
+{
+private:
+	Sprite Pickup;
+	Texture T1;
+	Texture T2;
+	Texture T3;
+	float xposition;
+	float yposition;
+	int s;
+
+public:
+	PickUps();
+	void setPickupposition(RenderWindow &window);
+
+	void draw(RenderWindow &window);
+	Sprite getBoundingRectangle();
+	void disapper() { Pickup.setPosition(-1000, -1000); }
+	int returns() { return s; }
+	void setpos(int x, int y)
+	{
+		xposition = x;
+		yposition = y;
+	}
+	bool PickupIntersect(Player &player, RenderWindow &window);
+	~PickUps();
+};
